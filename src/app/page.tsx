b3110f3a35c3e3d0d1113ab9d2fc413ab7328ef2
Your Name <you@example.com>
2025-01-05 00:01:00 +0500

    cafeflux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import Navbar from "../components/Navbar";
 import Carasouel from '@/components/Carasouel';
 import Banner from '@/components/Banner';
 import Progressor from '@/components/Progressor';
-import ProductList from '@/components/ProductList';
-import Footer from '@/components/Footer';
 import Coffee from '@/components/Coffee';
 import MenuItems from '@/components/MenuItems';
+import ProductList from '@/components/ProductList';
 
 
 export default function page() {
